0fd1e3b8f7a5acee5498a459336800c01b6e1b2e
dmitrysam88 <dmitry.sam88@gmail.com>
2018-06-16 23:39:16 +0300

    version 1.04

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from main.models import Exam, Question, Answer, Test
+from main.models import Exam, Question, Answer, Test, Subdivision, Worker
 
 # Register your models here.
 admin.site.register(Exam)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Test)
+admin.site.register(Subdivision)
+admin.site.register(Worker)
