a0e45eb73b22f6137db0b6b7428b571b44ea405f
dmitrysam88 <dmitry.sam88@gmail.com>
2018-06-18 23:25:00 +0300

    version 1.05

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import auth
-from main.models import Exam, Answer, Question, Test
+from main.models import Exam, Answer, Question, Test, Subdivision, Worker
 from django.contrib.auth.models import User
 from testing import choosing_questions
 import datetime
@@ -95,6 +95,11 @@ def showResults(request):
     else:
         test = Test.objects.all()
 
+    users = []
+    for foo in test:
+        users.append(foo.user)
+    workers = Worker.objects.all().filter(user__in=users)
+
     if dateTo == '2100-01-01':
         dateTo = None
     if dateFrom == '1980-01-01':
@@ -104,6 +109,7 @@ def showResults(request):
         'test':test,
         'dateFrom':dateFrom,
         'dateTo':dateTo,
+        'workers':workers,
     }
     return render(request, 'main/show_results.html', context)
 
@@ -118,6 +124,11 @@ def createFile(request):
         dateFrom = '1980-01-01'
     test = Test.objects.all().filter(time__range=(dateFrom, dateTo+" 23:59:59"))
 
+    users = []
+    for foo in test:
+        users.append(foo.user)
+    workers = Worker.objects.all().filter(user__in=users)
+
     #write data to xls file
     wb = openpyxl.Workbook()
     #wb.create_sheet('test')
@@ -126,16 +137,27 @@ def createFile(request):
     sheet['A1'] = '№'
     sheet['B1'] = 'Время'
     sheet['C1'] = 'Ползователь'
-    sheet['D1'] = 'Экзамен'
-    sheet['E1'] = 'Оценка'
+    sheet['D1'] = 'Имя'
+    sheet['E1'] = 'Фамилия'
+    sheet['F1'] = 'Номер'
+    sheet['G1'] = 'Подразделение'
+    sheet['H1'] = 'Экзамен'
+    sheet['I1'] = 'Оценка'
 
     numRow = 2
     for foo in test:
         sheet['A'+str(numRow)] = str(foo.id)
         sheet['B'+str(numRow)] = foo.time.strftime('%Y-%m-%d %H:%M')
         sheet['C'+str(numRow)] = foo.user.username
-        sheet['D'+str(numRow)] = foo.exam.name
-        sheet['E'+str(numRow)] = str(foo.mark)+'/'+str(foo.exam.number_questions)
+        for worker in workers:
+            if worker.user == foo.user:
+                sheet['D' + str(numRow)] = worker.first_name
+                sheet['E' + str(numRow)] = worker.last_name
+                sheet['F' + str(numRow)] = worker.number
+                sheet['G' + str(numRow)] = worker.subdivision.name
+
+        sheet['H'+str(numRow)] = foo.exam.name
+        sheet['I'+str(numRow)] = str(foo.mark)+'/'+str(foo.exam.number_questions)
         numRow += 1
 
     #sheet.Columns('A:E').EntireColumn.AutoFit()
